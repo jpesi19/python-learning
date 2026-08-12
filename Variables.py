1e858ace552f875a12902b197c5d91f547573e24
Jacob Pesina <jpesina19@gmail.com>
2026-08-11 22:50:04 -0500

    Variables File

---
+++ Variables.py
@@ -0,0 +1,18 @@
+#Python variables don't need to be tied down
+x = 5
+y = "John"
+print(x)
+print(y)
+#Py variables type can be changed
+a = 12
+a = "Sally"
+print(x)
+#Py casting like other languages
+a=str(3) # Will be '3'
+b=int(3) # Will be 3
+c=float(3) # Will be 3.0
+#How to get type of variable
+p=5
+q="Jake"
+print(type(p))
+print(type(q))
