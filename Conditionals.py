a28c25f9064eaac2421f49e1609dc762ab774d75
Jacob Pesina <jpesina19@gmail.com>
2026-08-15 00:50:08 -0500

    Practicing of conditionals

---
+++ Conditionals.py
@@ -0,0 +1,15 @@
+a=100
+b=200
+if b>a:
+    print("B is greather than A")
+
+AteBreakfast = True
+if AteBreakfast:
+    print("Breakfast is the most important meal of the day.")
+
+if a>b:
+    print("A is greater than B")
+elif a == b:
+    print("They are the same!")
+elif b>a:
+    print("B is greater than A 2!")
